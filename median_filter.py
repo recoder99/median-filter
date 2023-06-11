2c60f8a43bb557ad9f92dd35ef61a61e1be3efd6
recoder99 <roelccastro@iskolarngbayan.pup.edu.ph>
2023-06-11 23:51:57 +0800

    setting median filter window to 3x3

--- median_filter.py
+++ median_filter.py
@@ -2,9 +2,9 @@ import cv2 as cv
 import sys
 import math
 
-window_size = 9
+window_size = 3
 img = cv.imread("az.png")
-padded_img = cv.copyMakeBorder(img, 4, 4, 4, 4, cv.BORDER_REPLICATE, value=0)
+padded_img = cv.copyMakeBorder(img, 1, 1, 1, 1, cv.BORDER_REPLICATE, value=0)
 
 
 center_start = (window_size - 1)/2
@@ -18,19 +18,16 @@ for i in range(padded_img.shape[0] - window_size):
         for l in range(window_size):
             for k in range(window_size):
                 window.append(padded_img[l+i][k+j][0])
-        
-        padded_img[i+4][j+4] = (255,0,0)
-        cv.waitKey(1)
         #sort the values
         window.sort()
         #get the median
         median_value = window[math.ceil((pow(window_size, 2)-1)/2)]
         #replace the pixel with its median value
-        padded_img[i+4][j+4] = median_value
+        padded_img[i+1][j+1] = median_value
 
         #for demonstration purposes only
-        cv.imshow("live preview", padded_img)
-        cv.waitKey(1)
+        #cv.imshow("live preview", padded_img)
+        #cv.waitKey(1)
 
 
 cv.imshow("sdfsd", padded_img)
